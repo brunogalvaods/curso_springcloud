6ca60d17887c5f96133b4c1c7bb2f49bfcce4bc1
brunogalvaods <brunogalvaods@gmail.com>
2017-11-21 15:42:22 -0200

    Adicionando o authorization server.

---
+++ itemsharing/itemsharing.authorizationserver/src/main/java/com/itemsharing/authorizationserver/model/Authority.java
@@ -0,0 +1,23 @@
+package com.itemsharing.authorizationserver.model;
+
+import org.springframework.security.core.GrantedAuthority;
+
+public class Authority implements GrantedAuthority {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 3926044612721198120L;
+	
+	private final String authority;
+	
+	public Authority(String authority) {
+		this.authority = authority;
+	}
+	
+	@Override
+	public String getAuthority() {
+		return authority;
+	}
+
+}
